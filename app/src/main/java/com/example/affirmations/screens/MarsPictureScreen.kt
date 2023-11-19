5c7657440ce1ea0e843a944d164ad5e790f9ffb9
Louis-Desmet <louis.desmet2@student.hogent.be>
2023-11-19 16:51:40 +0100

    add errorscreen

--- app/src/main/java/com/example/affirmations/screens/MarsPictureScreen.kt
+++ app/src/main/java/com/example/affirmations/screens/MarsPictureScreen.kt
@@ -1,9 +1,12 @@
 package com.example.affirmations.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,8 +33,23 @@ when (marsUiState) {
     }
     is  MarsUiState.Error -> {
         //error
+        ErrorScreen( modifier = modifier.fillMaxSize())
     }
 }
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+    }
 }
 
 @Composable
