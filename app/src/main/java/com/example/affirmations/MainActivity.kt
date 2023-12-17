dd4c3bff3631676dc82a9115d5f6fac98a227fa5
Louis Desmet <71658043+Louis-Desmet@users.noreply.github.com>
2023-12-17 11:31:34 +0100

    Refactor

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -4,26 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.example.affirmations.ui.theme.AffirmationsTheme
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.*
-import com.example.affirmations.ui.MarsViewModel
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.affirmations.screens.APODScreen
-import com.example.affirmations.screens.MarsPictureScreen
 import com.example.affirmations.ui.APODViewModel
-import com.example.affirmations.ui.theme.Testtheme
-
+import com.example.affirmations.ui.MarsViewModel
+import com.example.affirmations.ui.NasaApp
+import com.example.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -31,62 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AffirmationsTheme {
-                val navController = rememberNavController()
+                // Instantiate your view models here
                 val viewModelMars: MarsViewModel by viewModels { MarsViewModel.Factory }
                 val apodViewModel: APODViewModel by viewModels {APODViewModel.Factory}
-                Scaffold(
-                    bottomBar = { BottomBar(navController) }
-                ) { innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = Destinations.APODScreen,
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        composable(Destinations.MarsPictureScreen) {
-                            AnimatedVisibility(
-                                visible = true,
-                                enter = fadeIn(),
-                                exit = fadeOut()
-                            ) {
-                                MarsPictureScreen(
-                                    viewModel = viewModelMars,
-                                    marsUiState = viewModelMars.marsUiState,
-                                    retryAction = viewModelMars::getMarsImages
-                                )
-                            }
-                        }
-                        composable(Destinations.APODScreen) {
-                            AnimatedVisibility(
-                                visible = true,
-                                enter = fadeIn(),
-                                exit = fadeOut()
-                            ) {
-                            //HelloWorldScreen()
-                            APODScreen(
-                                apodUiState =  apodViewModel.apodUiState, retryAction = apodViewModel::getAPOD
-                            )
-                        }
-                        }
-                    }
-                }
+
+                // Call NasaApp with the view models
+                NasaApp(viewModelMars, apodViewModel)
             }
         }
     }
 }
-@Composable
-fun BottomBar(navController: NavController) {
-    BottomAppBar {
-        NavigationBarItem(
-            icon = { Icon(Icons.Filled.Home, contentDescription = "APOD") },
-            selected = navController.currentDestination?.route == Destinations.APODScreen,
-            onClick = { navController.navigate(Destinations.APODScreen) }
-        )
-        NavigationBarItem(
-            icon = { Icon(Icons.Filled.List, contentDescription = "Mars Pictures") },
-            selected = navController.currentDestination?.route == Destinations.MarsPictureScreen,
-            onClick = { navController.navigate(Destinations.MarsPictureScreen) }
-        )
-
-    }
-}
-
---
+++ app/src/main/java/com/example/affirmations/ui/NasaApp.kt
@@ -0,0 +1,86 @@
+package com.example.affirmations.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.affirmations.Destinations
+import com.example.affirmations.screens.APODScreen
+import com.example.affirmations.screens.MarsPictureScreen
+import com.example.affirmations.ui.APODViewModel
+import com.example.affirmations.ui.MarsViewModel
+
+@Composable
+fun NasaApp(
+    viewModelMars: MarsViewModel,
+    apodViewModel: APODViewModel
+) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = { BottomBar(navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Destinations.APODScreen,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Destinations.MarsPictureScreen) {
+                AnimatedVisibility(
+                    visible = true,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    MarsPictureScreen(
+                        viewModel = viewModelMars,
+                        marsUiState = viewModelMars.marsUiState,
+                        retryAction = viewModelMars::getMarsImages
+                    )
+                }
+            }
+            composable(Destinations.APODScreen) {
+                AnimatedVisibility(
+                    visible = true,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    APODScreen(
+                        apodUiState = apodViewModel.apodUiState,
+                        retryAction = apodViewModel::getAPOD
+                    )
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun BottomBar(navController: NavController) {
+    BottomAppBar {
+        NavigationBarItem(
+            icon = { Icon(Icons.Filled.Home, contentDescription = "APOD") },
+            selected = navController.currentDestination?.route == Destinations.APODScreen,
+            onClick = { navController.navigate(Destinations.APODScreen) }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Filled.List, contentDescription = "Mars Pictures") },
+            selected = navController.currentDestination?.route == Destinations.MarsPictureScreen,
+            onClick = { navController.navigate(Destinations.MarsPictureScreen) }
+        )
+
+    }
+}
